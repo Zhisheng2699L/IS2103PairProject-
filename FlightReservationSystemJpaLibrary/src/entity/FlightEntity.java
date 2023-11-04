9b3c9feff53bf0ed40b546f2cbc0dcd8655f060a
Foo Zhi Sheng <e0958287@u.nus.edu>
2023-11-04 10:40:21 +0800

    Added some clients

---
+++ FlightReservationSystemJpaLibrary/src/entity/FlightEntity.java
@@ -0,0 +1,138 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+import static jdk.nashorn.internal.runtime.Debug.id;
+
+/**
+ *
+ * @author foozh
+ */
+@Entity
+public class FlightEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long flightId;
+    
+    @Column(nullable = false, unique = true, length = 8)
+    @Size(min = 3, max = 8)
+    private String flightNum;
+    
+    @Column(nullable = false)
+    private boolean disabled;
+
+    @ManyToOne(optional = false, cascade = CascadeType.DETACH)
+    @JoinColumn(nullable = false)
+    private FlightRouteEntity flightRoute;
+
+    @ManyToOne(optional = false, cascade = CascadeType.DETACH)
+    @JoinColumn(nullable = false)
+    private AircraftConfigurationEntity aircraftConfig;
+
+    @OneToMany(mappedBy = "flight", fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    private List<FlightSchedulePlanEntity> flightSchedulePlan;
+
+    public FlightEntity() {
+    }
+
+    public FlightEntity(Long FlightId, String flightNum, boolean disabled, FlightRouteEntity flightRoute, AircraftConfigurationEntity aircraftConfig, List<FlightSchedulePlanEntity> flightSchedulePlan) {
+        this.flightId = FlightId;
+        this.flightNum = flightNum;
+        this.disabled = disabled;
+        this.flightRoute = flightRoute;
+        this.aircraftConfig = aircraftConfig;
+        this.flightSchedulePlan = flightSchedulePlan;
+    }
+    
+    public Long getFlightId() {
+        return flightId;
+    }
+    
+    public String getFlightNum() {
+        return flightNum;
+    }
+
+    public void setFlightId(Long flightId) {
+        this.flightId = flightId;
+    }
+    
+    public void setFlightNum(String flightNum) {
+        this.flightNum = flightNum;
+    }
+    
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+    
+    public FlightRouteEntity getFlightRoute() {
+        return flightRoute;
+    }  
+
+    public void setFlightRoute(FlightRouteEntity flightRoute) {
+        this.flightRoute = flightRoute;
+    }
+    
+    public AircraftConfigurationEntity getAircraftConfig() {
+        return aircraftConfig;
+    }
+
+    public void setAircraftConfig(AircraftConfigurationEntity aircraftConfig) {
+        this.aircraftConfig = aircraftConfig;
+    }
+    
+    public List<FlightSchedulePlanEntity> getFlightSchedulePlan() {
+        return flightSchedulePlan;
+    }
+
+    public void setFlightSchedulePlan(List<FlightSchedulePlanEntity> flightSchedulePlan) {
+        this.flightSchedulePlan = flightSchedulePlan;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (flightId != null ? flightId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the FlightId fields are not set
+        if (!(object instanceof FlightEntity)) {
+            return false;
+        }
+        FlightEntity other = (FlightEntity) object;
+        if ((this.flightId == null && other.flightId != null) || (this.FlightId != null && !this.FlightId.equals(other.FlightId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Flight[ id=" + flightId + " ]";
+    }
+    
+}
---
+++ FlightReservationSystemJpaLibrary/src/entity/FlightRouteEntity.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Cacheable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author foozh
+ */
+@Entity
+@Cacheable(false)
+@Table(uniqueConstraints=
+       @UniqueConstraint(columnNames = {"origin", "destination"}))
+public class FlightRouteEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long flightrouteId;
+   
+    @Column(nullable = false)
+    @NotNull
+    private boolean disabled;
+
+    @OneToMany(mappedBy = "flightRoute", fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    private List<FlightEntity> flights;
+
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "origin")
+    private AirportEntity origin;
+
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "destination")
+    private AirportEntity destination;
+
+    public FlightRouteEntity() {
+    }
+
+    public FlightRouteEntity(Long flightrouteId, boolean disabled, List<FlightEntity> flights, AirportEntity origin, AirportEntity destination) {
+        this.flightrouteId = flightrouteId;
+        this.disabled = disabled;
+        this.flights = flights;
+        this.origin = origin;
+        this.destination = destination;
+    }
+    
+    public Long getFlightrouteId() {
+        return flightrouteId;
+    }
+
+    public void setFlightrouteId(Long flightrouteId) {
+        this.flightrouteId = flightrouteId;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public List<FlightEntity> getFlights() {
+        return flights;
+    }
+
+    public AirportEntity getOrigin() {
+        return origin;
+    }
+
+    public AirportEntity getDestination() {
+        return destination;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    public void setFlights(List<FlightEntity> flights) {
+        this.flights = flights;
+    }
+
+    public void setOrigin(AirportEntity origin) {
+        this.origin = origin;
+    }
+
+    public void setDestination(AirportEntity destination) {
+        this.destination = destination;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (flightrouteId != null ? flightrouteId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the FlightrouteId fields are not set
+        if (!(object instanceof FlightRouteEntity)) {
+            return false;
+        }
+        FlightRouteEntity other = (FlightRouteEntity) object;
+        if ((this.flightrouteId == null && other.flightrouteId != null) || (this.FlightrouteId != null && !this.FlightrouteId.equals(other.FlightrouteId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.FlightRouteEntity[ id=" + flightrouteId + " ]";
+    }
+    
+}
