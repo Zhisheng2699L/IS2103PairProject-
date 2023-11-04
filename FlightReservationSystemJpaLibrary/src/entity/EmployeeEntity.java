0c76d417cd938236b84a69d3db7b32253762c3f4
flylivekj <kahjyun@trinax.sg>
2023-11-04 10:01:18 +0800

    'hi'

---
+++ FlightReservationSystemJpaLibrary/src/entity/EmployeeEntity.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import enumeration.EmployeeAccessRightEnum;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author kahjy
+ */
+@Entity
+public class EmployeeEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private Long employeeId;
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    private String userName;
+    
+    private String password;
+    
+    @Enumerated(EnumType.STRING)
+    private EmployeeAccessRightEnum accessRight;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof EmployeeEntity)) {
+            return false;
+        }
+        EmployeeEntity other = (EmployeeEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.EmployeeEntity[ id=" + id + " ]";
+    }
+    
+}
